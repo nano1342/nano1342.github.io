c5e3501853c77f3c4c161700225208edf83953b3
nano1342 <arnaudbulteau@yahoo.fr>
2025-06-06 10:47:55 +0200

    seven segment text on card

--- js/render/nodes/card.js
+++ js/render/nodes/card.js
@@ -5,11 +5,22 @@ import {BoxBuilder} from '../geometry/box-builder.js';
 // import {TextureLoader} from '../../util/texture-loader.js';
 import {PrimitiveStream} from '../geometry/primitive-stream.js';
 import {PbrMaterial} from '../materials/pbr.js';
+import {SevenSegmentText} from './seven-segment-text.js';
 
 export class CardNode extends Node {
   constructor() {
     super();
     this._textureSize = 1; // Add this line to set a default size
+    this._sevenSegmentNode = new SevenSegmentText();
+    // Hard coded because it doesn't change:
+    // Scale by 0.075 in X and Y
+    // Translate into upper left corner w/ z = 0.02
+    this._sevenSegmentNode.matrix = new Float32Array([
+      0.075, 0, 0, 0,
+      0, 0.075, 0, 0,
+      0, 0, 1, 0,
+      -0.3625, 0.3625, 0.02, 1,
+    ]);
   }
 
   onRendererChanged(renderer) {
@@ -18,24 +29,29 @@ export class CardNode extends Node {
     stream.clear();
     stream.startGeometry();
 
-    stream.pushVertex(-hs, hs, 0, 0, 0, 0, 0, 1);
+    stream.pushVertex(-hs,  hs, 0, 0, 0, 0, 0, 1);
     stream.pushVertex(-hs, -hs, 0, 0, 1, 0, 0, 1);
-    stream.pushVertex(hs, -hs, 0, 1, 1, 0, 0, 1);
-    stream.pushVertex(hs, hs, 0, 1, 0, 0, 0, 1);
+    stream.pushVertex( hs, -hs, 0, 1, 1, 0, 0, 1);
+    stream.pushVertex( hs,  hs, 0, 1, 0, 0, 0, 1);
 
     stream.pushTriangle(0, 1, 2);
     stream.pushTriangle(0, 2, 3);
 
+
+
     stream.endGeometry();
 
-    let iconPrimitive = stream.finishPrimitive(renderer);
-    let boxMaterial = new PbrMaterial();
-    boxMaterial.baseColorFactor.value = [0.9, 0.9, 0.9, 1]; // grey
+    let primitive = stream.finishPrimitive(renderer);
+    let material = new PbrMaterial();
+    material.baseColorFactor.value = [0.9, 0.9, 0.9, 1]; // grey
     this._iconRenderPrimitive = renderer.createRenderPrimitive(
-      iconPrimitive,
-      boxMaterial
+      primitive,
+      material
     );
     this.addRenderPrimitive(this._iconRenderPrimitive);
+
+    this.addNode(this._sevenSegmentNode);
+    this._sevenSegmentNode.text = "ABCD";
   }
 }
 
